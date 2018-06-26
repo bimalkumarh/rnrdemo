31e1d87f9995931f2b25e24df8406bd10e3ab952
unknown <bimalkumar@gmail.com>
2018-06-26 22:52:15 +0530

    email feature added

---
+++ middleware/forgotPasswordEmail.js
@@ -0,0 +1,72 @@
+var nodemailer = require('nodemailer');
+var handlebars = require('handlebars');
+var fs = require('fs');
+var connection = require("../database");
+
+var readHTMLFile = function (path, callback) {
+    fs.readFile(path, { encoding: 'utf-8' }, function (err, html) {
+        if (err) {
+            throw err;
+            callback(err);
+        }
+        else {
+            callback(null, html);
+        }
+    });
+};
+
+var forgotPasswordEmail = function (req, res) {
+
+    var email = req.body.email;
+
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            user: 'x7k6odpowzief7ao@ethereal.email', // generated ethereal user
+            pass: 'w7CUcz2aAXGgm9begP' // generated ethereal password
+        }
+    });
+
+    var query = "SELECT * FROM user WHERE email=?";
+    connection.query(query, [email], function (err, rows) {
+        if (err) {
+            res.json({ "Error": true, "Message": "Error executing MySQL query" });
+        } else {
+            if (rows.length == 1) {
+                readHTMLFile(__dirname + '/emailforgotpw.html', function (err, html) {
+                    var template = handlebars.compile(html);
+                    var replacements = {
+                        url: "www.example.com/somepage?userid="+rows[0].user_id
+                    };
+                    var htmlToSend = template(replacements);
+                    console.log("htmlToSend", htmlToSend);
+                    var mailOptions = {
+                        from: "bimalk.inapp@gmail.com",
+                        to: email,
+                        subject: 'Forgot Password',
+                        html: htmlToSend
+                    };
+                    transporter.sendMail(mailOptions, function (error, response) {
+                        if (error) {
+                            console.log(error);
+                            callback(error);
+                        }
+                        console.log('response', response);
+                        res.send("mail sent");
+                    });
+                });
+            } else {
+                res.json({ "Error": true, "Message": "Email not found" });
+            }
+        }
+    });
+
+
+
+
+
+
+};
+
+module.exports = forgotPasswordEmail;
